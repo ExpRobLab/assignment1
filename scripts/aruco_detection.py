Implement the logic to turn to the marker, need a fix to the x and y variable erroro computation

scripts/aruco_detection.py
@@ -6,121 +6,111 @@ from rclpy.node import Node
 import numpy as np
 import cv2
 import imutils
-import tf_transformations as tf
+import math
 
 from geometry_msgs.msg import Twist
 from aruco_opencv_msgs.msg import ArucoDetection
 from sensor_msgs.msg import CompressedImage
-from tf2_ros import Buffer, TransformListener, LookupException, ConnectivityException, ExtrapolationException
+from tf2_ros import Buffer, TransformListener, TransformException, LookupException, ConnectivityException, ExtrapolationException
 
 VERBOSE = False
 
 
-class ImageFeature(Node):
+class ArucoDetector(Node):
     def __init__(self):
-        super().__init__('image_feature')
-
-        # Publishers
-        self.vel_pub = self.create_publisher(
-            Twist,
-            '/cmd_vel',
-            1
-        )
-
-        # Subscriber
-        self.subscriber = self.create_subscription(
-            CompressedImage,
-            '/camera/image/compressed',
-            self.callback,
-            1
-        )
-
-        #Aruco Subscriber
-        self.subscriber = self.create_subscription(
-            ArucoDetection,
-            '/aruco_detections',
-            self.detection_callback,
-            1
-        )
-
-        self.detected_markers = []
-        self.marker_msgs = []
-        self.state = 'discover'
-        self.current_tarket_index = 0
-
-        self.timer = self.create_timer(0.1, self.control_loop)
-
-         # Buffer TF + listener
+        super().__init__('aruco_detection_node')
+        
+        # Listeners
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
 
+        # Subscribers
+        self.subscriber = self.create_subscription(ArucoDetection,'/aruco_detections',self.__detection_callback,1)
+        self.detected_markers: dict = {}
+
+        # Publishers cmd velocity
+        self.vel_pub = self.create_publisher(Twist,'/cmd_vel',1)
+        self.timer = self.create_timer(0.01, self.__control)
+
+        # TODO Publisher img of the marker
+        
+
+        self.state = "detecting"
+        self.treshold = 4.0  # radiants
+        self.Kp = 1.0
+        self.max_angular = 1.0
+
+        self.robot_cmd_vel: Twist = Twist()
+        self.robot_cmd_vel.angular.z = 0.5  # Initial angular velocity
 
 
-    def detection_callback(self, msg):
-        self.marker_msgs = msg.markers
-        if self.state == 'discover':
 
-            for marker in msg.markers:
-                if marker.marker_id not in self.detected_markers and len(self.detected_markers) < 5:
+    def __detection_callback(self, msg):
+        if self.state != "detecting":
+            return
+        
+        self.vel_pub.publish(self.robot_cmd_vel)
 
-                    self.get_logger().info(f"Detected Marker: {marker.marker_id}")
-                    transform = self.tf_buffer.lookup_transform(
-                        'odom',
-                        'camera_link_optical',
-                        rclpy.time.Time()
-                    )
-                    marker_position = np.array(marker.pose.position)
-                    marker.pose.orientation
+        for marker in msg.markers:
+            frame_id = f"marker_{marker.marker_id}"
 
-                    R = tf.quaternion_matrix(marker.pose.orientation)
+            try:
+                odom_T_arucomarker = self.tf_buffer.lookup_transform(
+                    'odom',
+                    frame_id,
+                    rclpy.time.Time()
+                )
+                self.detected_markers[frame_id] = odom_T_arucomarker
+                
+                if len(self.detected_markers) == 5:
+                    self.detected_markers = dict(sorted(self.detected_markers.items()))
+                    self.state = "centering"
 
-                    T = np.eye(4)
-                    T[0:2,0:2] = R
-                    T[0:2,3] = marker_position.reshape(3,1)
-                    
-                    print(T)
+                if frame_id not in self.detected_markers:
+                    self.get_logger().info(f"Detected new marker: {frame_id}")
 
-                    self.detected_markers.append(marker.marker_id)
+            except TransformException:
+                continue
     
-    def control_loop(self):
-        twist = Twist()
+    
+    def __control(self):
+        if self.state != "centering":
+            return
+        
+        marker_id, odom_T_arucomarker = next(iter(self.detected_markers.items()))
 
-        if self.state == 'discover':
-            if len(self.detected_markers) < 5:
-                twist.angular.z = 0.5
-            else:
-                self.get_logger().info("All Markers Detected!")
-                self.detected_markers.sort()
-                self.state = 'show_ordered'
-                self.current_tarket_index = 0
+        odom_T_camera = self.tf_buffer.lookup_transform(
+                    'odom',
+                    'camera_link_optical',
+                    rclpy.time.Time()
+                )
+        
+        x = odom_T_arucomarker.transform.translation.x - odom_T_camera.transform.translation.x
+        y = odom_T_arucomarker.transform.translation.y - odom_T_camera.transform.translation.y
+        angle_to_marker = math.atan2(y, x)
 
-        elif self.state == 'show_ordered':
-            if not self.marker_msgs:
-                twist.angular.z = 0.5
-            else: 
-                tarket_id = self.detected_markers[self.current_tarket_index]
-                ids_visible = [m.marker_id for m in self.marker_msgs]
-                if tarket_id in ids_visible:
-                    twist.angular.z = 0.0
-                    self.get_logger().info(f"Marker Visualized: {tarket_id}")
+        self.robot_cmd_vel.angular.z = max(-self.max_angular, min(self.max_angular, self.Kp * angle_to_marker))
+        self.vel_pub.publish(self.robot_cmd_vel)
 
-                    self.current_tarket_index += 1
-                    if self.current_tarket_index >= len(self.detected_markers):
-                        self.get_logger().info("Mission Complited")
-                        self.timer.cancel()
-                        twist.angular.z = 0.0
-                    
-                else:
-                    twist.angular.z = 0.5
-        self.vel_pub.publish(twist)
+        # Remove the element from the dictionary
+        if abs(angle_to_marker) < self.treshold:
+            self.robot_cmd_vel.angular.z = 0.0
+            self.vel_pub.publish(self.robot_cmd_vel)
 
-
-
+            self.get_logger().info(f"Pointed marker: {marker_id}\n")
+            # TODO self.get_logger().info("Pres <butto> to continue...\n")
+            self.detected_markers.pop(marker_id)
+        
+        if not self.detected_markers:
+            self.robot_cmd_vel.angular.z = 0.5
+            self.vel_pub.publish(self.robot_cmd_vel)
+            self.state = "detecting"
 
 def main(args=None):
     rclpy.init(args=args)
-    node = ImageFeature()
+
     try:
+        node = ArucoDetector()
         rclpy.spin(node)
     except KeyboardInterrupt:
         node.get_logger().info("Shutting down ImageFeature node")